d81c0a6e1bfb5a729d223046c9584f779832bc58
Maildes <vlad.mailyk8@gmailcom>
2020-04-26 15:53:10 +0300

    Updates

---
+++ precache-manifest.3c1c205a2f8aacb59621a4e849e4b30b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74192bd330dbbe0fafb4bb0a85894e2f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9e694fce9d3b6d60262c",
+    "url": "./static/css/main.407ef130.chunk.css"
+  },
+  {
+    "revision": "72447c2ea5c6ffb84bab",
+    "url": "./static/js/2.82f051fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.82f051fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e694fce9d3b6d60262c",
+    "url": "./static/js/main.10800b62.chunk.js"
+  },
+  {
+    "revision": "1befd8f6ec08271c40e1",
+    "url": "./static/js/runtime-main.764b2130.js"
+  }
+]);
